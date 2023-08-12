94ceda02bb23f0832fab6bb8889b087e0c98bf23
Colin Robinson <colin@Colins-Air.lan>
2023-08-12 13:09:30 -0500

    Finished Task 3 and started work on Task 4.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Banner from "./components/Banner";
 import { useState } from "react";
 import satData from "./components/satData";
 
+const filterByType = (currentType) => {
+  const displaySats = satData.filter((newSatDisplay) => {
+    return newSatDisplay.orbitType === currentType;
+  });
+  setSat(displaySats);
+};
+
 function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   return (
     <div>
       <Banner />
-      <Buttons />
-      <Table />
+      <Buttons
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats} 
+      />
+      <Table sat={sat} />
     </div>
   );
 }
